e66d504267145ebc26f1a83d42841ddd9bbfe91f
Mihaly Toth <Mihaly.Toth@kuka.com>
2021-06-19 21:32:11 +0200

    EmployeesApplication first commit

--- employees/src/main/java/employees/EmployeesApplication.java
+++ employees/src/main/java/employees/EmployeesApplication.java
@@ -2,6 +2,7 @@ package employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeesApplication {
@@ -9,5 +10,10 @@ public class EmployeesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
 	}
+
+	//@Bean
+	//public HelloService helloService() {
+	//	return new HelloService();
+	//}
 
 }
