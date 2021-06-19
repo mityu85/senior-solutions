734733f77aa82a4cb42a21a81661c3b0e303fdbb
Mihaly Toth <Mihaly.Toth@kuka.com>
2021-06-19 14:32:34 +0200

    minor test changes

--- locations-solution/src/main/java/locations/DistanceService.java
+++ locations-solution/src/main/java/locations/DistanceService.java
@@ -4,26 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class DistanceService implements LocationRepository {
+public class DistanceService {
 
-    private LocationParser locationParser = new LocationParser();
-    private List<Location> locations;
+    LocationRepository locationRepository;
+    LocationParser locationParser = new LocationParser();
 
-    public DistanceService(List<Location> locations) {
-        this.locations = locations;
+    public DistanceService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
     }
 
     public Optional<Double> calculateDistance(String name1, String name2) {
-        if (findByName(name1).isPresent() && findByName(name2).isPresent()) {
-            System.out.println("OK");
-            return Optional.of(locationParser.distanceFrom(findByName(name1).get(), findByName(name2).get()));
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Location> findByName(String name) {
-        Optional<Location> location = locations.stream().filter(l -> l.getName().equals(name)).findAny();
-        return location;
+        Optional<Double> result = Optional.of(locationParser.distanceFrom(locationRepository.findByName(name1).get(),
+                locationRepository.findByName(name2).get()));
+        return result;
     }
 }
--- locations-solution/src/test/java/locations/DistanceServiceTest.java
+++ locations-solution/src/test/java/locations/DistanceServiceTest.java
@@ -1,43 +1,35 @@
 package locations;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DistanceServiceTest {
 
-    @BeforeEach
-    void init() {
-        service = new DistanceService(List.of(
-                new Location("Budapest", 14.789, 23.487),
-                new Location("Szeged", 14.789, 23.487)
-        ));
-    }
-
     @Mock
-    LocationRepository repository;
+    LocationRepository locationRepository;
 
     @InjectMocks
-    DistanceService service;
+    DistanceService distanceService;
 
     @Test
     void testCalculateDistance() {
-        //Optional<Double> result = service.calculateDistance("Budapest", "Szeged");
-        //when(repository.findByName(anyString()))
-        //        .thenReturn(Optional.of(new Location("Budapest", 14.789, 23.487)));
-        Optional<Location> result = service.findByName("Budapest");
-        assertEquals("Location{name='Budapest', lat=14.789, lon=23.487}", result.get().toString());
+
+        when(locationRepository.findByName(eq("Budapest")))
+                .thenReturn(Optional.of(new Location("Budapest", 15.789,78.789)));
+
+        Optional<Location> location = locationRepository.findByName("Budapest");
+        //Optional<Double> distance = distanceService.calculateDistance("Budapest", "Szeged");
+        verify(locationRepository).findByName(any());
+        assertEquals("Budapest", location.get().getName());
     }
 }
