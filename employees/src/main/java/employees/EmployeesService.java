424661aed2e1ad77d2435bdde8108bc2ef3385cc
Mihaly Toth <Mihaly.Toth@kuka.com>
2021-07-26 00:28:13 +0200

    Spring Boot Jpa

--- employees/src/main/java/employees/EmployeesService.java
+++ employees/src/main/java/employees/EmployeesService.java
@@ -1,71 +1,105 @@
 package employees;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
-import org.modelmapper.internal.bytebuddy.description.method.MethodDescription;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.reflect.Type;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class EmployeesService {
 
     private ModelMapper modelMapper;
-    private AtomicLong idGenerator = new AtomicLong();
+    private EmployeeRepository repository;
 
-    private List<Employee> employees = Collections.synchronizedList(new ArrayList<>(List.of(
-            new Employee(idGenerator.incrementAndGet(), "John Doe"),
-            new Employee(idGenerator.incrementAndGet(), "Jack Doe")
-    )));
+    //    private AtomicLong idGenerator = new AtomicLong();
+//
+//    private List<Employee> employees = Collections.synchronizedList(new ArrayList<>(List.of(
+//            new Employee(idGenerator.incrementAndGet(), "John Doe"),
+//            new Employee(idGenerator.incrementAndGet(), "Jack Doe")
+//    )));
 
-    public EmployeesService(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
+//    public EmployeesService(ModelMapper modelMapper) {
+//        this.modelMapper = modelMapper;
+//    }
+//
+//    public List<EmployeeDto> listEmployees(Optional<String> prefix) {
+//        List<Employee> filtered = employees.stream()
+//                .filter(e -> prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
+//                .collect(Collectors.toList());
+//        //Type targetListType = new TypeToken<List<EmployeeDto>>(){}.getType();
+//        //return modelMapper.map(filtered,targetListType);
+//        return filtered.stream().map(e -> modelMapper.map(e, EmployeeDto.class)).collect(Collectors.toList());
+//    }
 
     public List<EmployeeDto> listEmployees(Optional<String> prefix) {
-        List<Employee> filtered = employees.stream()
-                .filter(e -> prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
+        return repository.findAll().stream()
+                .map(e -> modelMapper.map(e, EmployeeDto.class))
                 .collect(Collectors.toList());
-        //Type targetListType = new TypeToken<List<EmployeeDto>>(){}.getType();
-        //return modelMapper.map(filtered,targetListType);
-        return filtered.stream().map(e -> modelMapper.map(e, EmployeeDto.class)).collect(Collectors.toList());
     }
+
+//    public EmployeeDto findEmployeeById(long id) {
+//        return modelMapper.map(employees.stream()
+//                .filter(e -> e.getId() == id).findAny()
+//                .orElseThrow(() -> new EmployeeNotFoundException(id)),
+//                EmployeeDto.class);
+//    }
 
     public EmployeeDto findEmployeeById(long id) {
-        return modelMapper.map(employees.stream()
-                .filter(e -> e.getId() == id).findAny()
-                .orElseThrow(() -> new EmployeeNotFoundException(id)),
-                EmployeeDto.class);
+        return modelMapper.map(repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found")), EmployeeDto.class);
     }
+
+//    public EmployeeDto createEmployee(CreateEmployeeCommand command) {
+//        Employee employee = new Employee(idGenerator.incrementAndGet(), command.getName());
+//        employees.add(employee);
+//        return  modelMapper.map(employee, EmployeeDto.class);
+//    }
 
     public EmployeeDto createEmployee(CreateEmployeeCommand command) {
-        Employee employee = new Employee(idGenerator.incrementAndGet(), command.getName());
-        employees.add(employee);
-        return  modelMapper.map(employee, EmployeeDto.class);
+        Employee employee = new Employee((command.getName()));
+        repository.save(employee);
+        return modelMapper.map(employee, EmployeeDto.class);
     }
 
+//    public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
+//        Employee employee = employees.stream()
+//                .filter(e -> e.getId() == id)
+//                .findFirst()
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//        employee.setName(command.getName());
+//        return modelMapper.map(employee, EmployeeDto.class);
+//    }
+
+    @Transactional
     public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
-        Employee employee = employees.stream()
-                .filter(e -> e.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new EmployeeNotFoundException(id));
+        Employee employee = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Employee not found"));
         employee.setName(command.getName());
         return modelMapper.map(employee, EmployeeDto.class);
     }
 
+//    public void deleteEmployee(long id) {
+//        Employee employee = employees.stream()
+//                .filter(e -> e.getId() == id)
+//                .findFirst()
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//        employees.remove(employee);
+//    }
+
     public void deleteEmployee(long id) {
-        Employee employee = employees.stream()
-                .filter(e -> e.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new EmployeeNotFoundException(id));
-        employees.remove(employee);
+        repository.deleteById(id);
     }
 
+//    public void deleteAllEmployees() {
+//        idGenerator = new AtomicLong();
+//        employees.clear();
+//    }
+
     public void deleteAllEmployees() {
-        idGenerator = new AtomicLong();
-        employees.clear();
+        repository.deleteAll();
     }
 }
